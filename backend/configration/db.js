de854f1ed18b10c16f85564e6c6740953ecb9ba2
praam <praa.m.2002@gmail.com>
2023-06-23 16:02:37 +0300

    init

--- backend/configration/db.js
+++ backend/configration/db.js
@@ -1,8 +1,7 @@
 const {MongoClient} = require('mongodb')
 
-const _uri = "mongodb://127.0.0.1:27017"
 const dbConnection = (collection, cb) => {
-    MongoClient.connect(_uri)
+    MongoClient.connect(process.env.DB_URI)
         .then(async client => {
             const db = client.db('MarketManagement').collection(collection)
             await cb(db)
